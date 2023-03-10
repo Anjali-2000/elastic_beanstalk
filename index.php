2eec80acabd05070b11d73b68d0b928ae56becf1
Anjali Tarachand Chavan <69081245+Anjali-2000@users.noreply.github.com>
2023-03-10 19:25:35 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>AWS DevOps</title>
+  <style>
+    body {
+      background-color: #e8f80b;
+    }
+  </style>
+</head>
+<body>
+  <div align="center">
+    <h1>
+      <?php
+      echo "Hello World!";
+      ?>
+    </h1>
+  </div>
+</body>
+</html>
